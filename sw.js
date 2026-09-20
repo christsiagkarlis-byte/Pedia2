459aca2a702fb42f9d00e40dc1bafe3a5b7aae41
christsiagkarlis-byte <christsiagkarlis@gmail.com>
2026-09-20 17:03:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "paizomath-v4";
+const CACHE_NAME = "paizomath-v5";
 const APP_SHELL = ["./", "./index.html", "./app/index.html", "./manifest.webmanifest", "./paizomath-icon.svg", "./assets/index-BT5Zs9ye.js", "./assets/index-VvnKvMSq.css", "./assets/question-banks.json"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
